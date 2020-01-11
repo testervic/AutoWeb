edac760e59d4f3c2f1cc29843f6481d87c4dc82e
testervic <469858846@qq.com>
2020-01-11 15:49:28 +0800

    fix

--- AutoWeb.sh
+++ AutoWeb.sh
@@ -16,9 +16,21 @@ if [[ "$result" != "" ]]
   then
 		echo "$result"
 		echo "运行成功"
+		cp /Users/vic/.jenkins/workspace/GetAutoWebCode/Report/ReportDetail.xlsx /Users/vic/.jenkins/workspace/RunAutoWeb/
+		cp /Users/vic/.jenkins/workspace/GetAutoWebCode/Log/*chrome*/outPut.log /Users/vic/.jenkins/workspace/RunAutoWeb/
+		cp /Users/vic/.jenkins/workspace/GetAutoWebCode/Log/*chrome*/*.png /Users/vic/.jenkins/workspace/RunAutoWeb/
+		rm -rf /Users/vic/.jenkins/workspace/GetAutoWebCode/Report/ReportDetail.xlsx
+		rm -rf /Users/vic/.jenkins/workspace/GetAutoWebCode/Log/*chrome*/outPut.log
+		rm -rf /Users/vic/.jenkins/workspace/GetAutoWebCode/Log/*chrome*/*.png
 		exit 0
   else
     echo "$result"
 		echo "运行失败"
+		cp /Users/vic/.jenkins/workspace/GetAutoWebCode/Report/ReportDetail.xlsx /Users/vic/.jenkins/workspace/RunAutoWeb/
+		cp /Users/vic/.jenkins/workspace/GetAutoWebCode/Log/*chrome*/outPut.log /Users/vic/.jenkins/workspace/RunAutoWeb/
+		cp /Users/vic/.jenkins/workspace/GetAutoWebCode/Log/*chrome*/*.png /Users/vic/.jenkins/workspace/RunAutoWeb/
+		rm -rf /Users/vic/.jenkins/workspace/GetAutoWebCode/Report/ReportDetail.xlsx
+		rm -rf /Users/vic/.jenkins/workspace/GetAutoWebCode/Log/*chrome*/outPut.log
+		rm -rf /Users/vic/.jenkins/workspace/GetAutoWebCode/Log/*chrome*/*.png
 		exit 1
 fi
